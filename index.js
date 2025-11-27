26bffb561b646a76050211866bcd69b993c78ab0
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2025-11-27 08:06:58 +0500

    feat: add tweet input area and handle tweet submission

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import { tweetsData } from "./data.js";
 
-console.log(tweetsData);
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+tweetBtn.addEventListener("click", function(){
+    console.log(tweetInput.value);
+})
